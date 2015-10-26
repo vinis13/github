8b72cf211910ff811650c0e0edbea8df9741cc25
vinis13 <cruzmendes@live.com>
2015-10-26 14:42:31 -0200

    codigo temperatur livro
    tste de upload

---
+++ calculadora simpries.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/*meu primeiro programa em c */
+
+int main ()
+
+{
+	float num1, num2;
+	char op;
+		 	
+	printf("digite: o numero / operacao / numero");
+	scanf("%f %c %f", &num1, &op, &num2 );
+	
+	switch (op)
+	{
+		case '+':
+			printf(" = %f\n", num1+num2);
+			break;
+		case '-':
+			printf(" = %f\n", num1+num2);
+			break;
+		case '*':
+			printf(" = %f\n", num1*num2);
+			break;
+		case '/':
+			if (num1 != 0)
+			printf(" = %15f\n", num1/num2);
+			else
+			printf("dividao por zero\n");
+			break;
+		defalt:
+			printf("operador invalido\n");
+			break;
+			
+	
+	}
+	
+	
+	
+}
+
